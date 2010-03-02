0bb5481803714561d46cea87f16cc294526c5f75
Ulrik Rasmussen <ulrik@squish.(none)>
2010-03-02 14:36:51 +0100

    some parsing done

--- interp/src/Language.hs
+++ interp/src/Language.hs
@@ -3,20 +3,25 @@ module Language(Expr(..), Pat(..), Join(..), Def(..), Proc(..)) where
 data Expr = VarE String
           | ZeroE
           | SuccE Expr
+    deriving (Show, Eq)
 
 data Pat  = VarP String
           | ZeroP
           | SuccP Pat
+    deriving (Show, Eq)
 
 data Join = VarJ String [Pat]
           | AndJ Join Join
+    deriving (Show, Eq)
 
 data Def  = EmptyD
           | ReactionD Join Proc
           | OrD Def Def
+    deriving (Show, Eq)
 
 data Proc = InertP
           | MsgP String [Expr]
           | AndP Proc Proc
           | DefP Def Proc
           | MatchP [Expr] [([Pat], Proc)]
+    deriving (Show, Eq)
--- interp/src/Parser.hs
+++ interp/src/Parser.hs
@@ -1,7 +1,30 @@
 module Parser() where
 
+import Language
+
 import Control.Monad
 import Control.Applicative
 import Text.ParserCombinators.Parsec hiding ((<|>), many, optional)
 
+lexeme p = p <* many space
 
+parens p = char '(' *> many space *> p <* char ')'
+
+identifier :: Parser String
+identifier = (:) <$> lower <*> many (upper<|>lower)
+
+expr :: Parser Expr
+expr =   ZeroE <$ (lexeme $ char 'Z')
+     <|> SuccE <$ (lexeme $ char 'S') <*> expr
+     <|> VarE  <$> (lexeme identifier)
+     <?> "Expression"
+
+pat :: Parser Pat
+pat  =   ZeroP <$ (lexeme $ char 'Z')
+     <|> SuccP <$ (lexeme $ char 'S') <*> pat
+     <|> VarP  <$> (lexeme identifier)
+     <?> "Pattern"
+
+join :: Parser Join
+join =   VarJ <$> identifier <*> (parens $ pat `sepBy` char ',')
+     <|> AndJ 
