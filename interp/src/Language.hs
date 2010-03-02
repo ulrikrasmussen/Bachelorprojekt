82cb5daf53ca5bd92c27dffe741d8e2699c2e2c3
Ulrik Rasmussen <ulrik@squish.(none)>
2010-03-02 13:41:07 +0100

    Defined the AST

--- interp/src/Language.hs
+++ interp/src/Language.hs
@@ -1,3 +1,22 @@
-module Language() where
+module Language(Expr(..), Pat(..), Join(..), Def(..), Proc(..)) where
 
+data Expr = VarE String
+          | ZeroE
+          | SuccE Expr
 
+data Pat  = VarP String
+          | ZeroP
+          | SuccP Pat
+
+data Join = VarJ String [Pat]
+          | AndJ Join Join
+
+data Def  = EmptyD
+          | ReactionD Join Proc
+          | OrD Def Def
+
+data Proc = InertP
+          | MsgP String [Expr]
+          | AndP Proc Proc
+          | DefP Def Proc
+          | MatchP [Expr] [([Pat], Proc)]
--- interp/src/Parser.hs
+++ interp/src/Parser.hs
@@ -3,3 +3,5 @@ module Parser() where
 import Control.Monad
 import Control.Applicative
 import Text.ParserCombinators.Parsec hiding ((<|>), many, optional)
+
+
