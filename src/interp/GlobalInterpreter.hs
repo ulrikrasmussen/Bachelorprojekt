64890cf0a22dc5c80505dd327c05b17fc21c8f79
Philip Lykke Carlsen <plcplc@gmail.com>
2010-05-08 01:55:19 +0200

    Added physical unit simulation with unreliable channels

--- src/interp/GlobalInterpreter.hs
+++ src/interp/GlobalInterpreter.hs
@@ -36,26 +36,38 @@ takeElem p xs = takeElem' [] xs
 rootLocation = "@root"
 
 
-runInterpreter :: InterpConfig -> Proc -> IO [Context]
-runInterpreter conf (Proc as) = do
-  (stdGen1, stdGen2) <- R.split <$> R.getStdGen
-  runInterpreter' stdGen2 0 [initContext [] as rootLocation rootLocation [] stdGen1]
-   where runInterpreter' stdGen n contexts = do
-           (stdGen', stdGen'') <- return $ R.split stdGen
+runInterpreter :: InterpConfig -> IO [Context]
+runInterpreter conf = let
+    comP = M.fromList [ p | x <- M.keys $ comLinks conf, 
+                            y <- M.keys $ comLinks conf, 
+                            p <- [((x,y), comProb (comLinks conf) x y)]]
+  in do 
+    (stdGen1, stdGen2) <- R.split <$> R.getStdGen
+    -- Create the initial "machines"
+    runInterpreter' comP stdGen2 0 $ mkInitialCtxs stdGen1 (machineClasses conf) (initialMachines conf) -- [initContext [] as rootLocation rootLocation [] stdGen1]
+  where runInterpreter' comP stdGen n contexts = do
+           (stdGen_, stdGen') <- return $ R.split stdGen
+           (stdGen__, stdGen'') <- return $ R.split stdGen_
+           (stdGen___, stdGen''') <- return $ R.split stdGen__
+           (stdGen'''', stdGen''''') <- return $ R.split stdGen___
            newAtms <- runExternals $ manipulators conf
            contexts' <- mapM (runApi $ apiMap conf) contexts >>=
                         (concatMap (heatLocations stdGen'') >>>
                          registerFail >>>
                          map halt >>>
-                         killFailed >>>
+                         killFailed comP stdGen''' >>>
                          map migrate >>>
-                         exchangeMessages >>>
-                         putMessages newAtms >>>
+                         exchangeMessages comP stdGen'''' >>>
+                         putMessages comP stdGen''''' [tagged| na <- newAtms, tagged <- [(rootLocation,na)]]  >>>
                          map (execInterp conf) >>>
                          return)
            if maybe (contexts /= contexts') (n/=) (breakAt conf)
-                then runInterpreter' stdGen' (n+1) contexts'
+                then runInterpreter' comP stdGen' (n+1) contexts'
                 else return contexts
+
+        mkInitialCtxs      _   _     [] = []
+        mkInitialCtxs stdGen cls (m:ms) = let (stdGen, stdGen') = R.split stdGen 
+          in (initContext [] [(M.findWithDefault undefined (mcClass m) cls)] (mcName m) rootLocation [] stdGen):(mkInitialCtxs stdGen' cls ms)
 
 {-
  - There are two types of "magic" devices:
@@ -88,12 +100,11 @@ halt context =
           isHalt _ = False
 
 -- |Propagates failure to subcontexts, and removes failed
-killFailed :: [Context] -> [Context]
-killFailed cs =
+killFailed :: M.Map (String, String) Double -> R.StdGen -> [Context] -> [Context]
+killFailed comP rGen cs =
    let (failed, ok) = propagateFail cs
-       conts = concatMap cFailureConts failed
-       msgs = map (\n -> MsgA n []) conts
-    in putMessages msgs ok
+       msgs = concatMap (\ctx -> [tagged| fc <- cFailureConts ctx, tagged <- [(cLocation ctx, MsgA fc [])]]) failed
+    in putMessages comP rGen msgs ok
      where propagateFail cs =
              let (failed, ok) = partition cFail cs
                  failNames = S.fromList $ map cLocation failed
@@ -162,28 +173,36 @@ heatLocations stdGen context =
             in initContext ds as name locParent (S.toList exports) stdGen
 
 -- | Exchanges messages between a list of contexts.
-exchangeMessages :: [Context] -> [Context]
-exchangeMessages cs =
+exchangeMessages :: M.Map (String, String) Double -> R.StdGen -> [Context] -> [Context]
+exchangeMessages comP rGen cs =
   let (cs', ms) = second concat . unzip $ map takeMessages cs
-   in putMessages ms cs'
+   in putMessages comP rGen ms cs'
 
-takeMessages ::  Context -> (Context, [Atom])
+takeMessages ::  Context -> (Context, [(String, Atom)])
 takeMessages context =
   let dvs = S.unions . map definedVars $ cDefs context
       (locals, nonlocals) = partition (isLocal dvs) $ cAtoms context
       exports = S.unions $ map getExports nonlocals
       context' = context { cAtoms = locals
                          , cExportedNames = cExportedNames context `S.union` exports }
-   in (context', nonlocals)
+   in (context', [tagged| nl <- nonlocals, tagged <- [(cLocation context, nl)]])
     where getExports (MsgA _ es) = S.unions $ map freeVars es
 
-putMessages ::  [Atom] -> [Context] -> [Context]
-putMessages ms [] = []
-putMessages ms (context:cs) =
- let dvs = S.unions . map definedVars $ cDefs context
-     (locals, ms') = partition (isLocal dvs) $ ms
-     context' = context { cAtoms = cAtoms context ++ locals }
-  in context':putMessages ms' cs
+putMessages :: M.Map (String, String) Double -> R.StdGen -> [(String, Atom)] -> [Context] -> [Context]
+putMessages comP rGen ms [] = []
+putMessages comP rGen ms (context:cs) =
+ let (rGen, rGen') = R.split rGen
+     dvs = S.unions . map definedVars $ cDefs context
+     (locals, ms') = partition (snd >>> isLocal dvs) $ ms
+     succCom = tryCom rGen locals
+     context' = context { cAtoms = cAtoms context ++ succCom }
+  in context':putMessages comP rGen' ms' cs
+  where
+    tryCom _  [] = []
+    tryCom rg ((loc,a):ls) = case M.lookup (loc, cLocation context) comP of
+      Nothing -> tryCom rg ls
+      Just cP -> let (p,rg') = R.randomR (0,1) rg in
+                 if cP >= p then a:tryCom rg' ls else tryCom rg' ls
 
 isLocal ::  S.Set String -> Atom -> Bool
 isLocal dvs (MsgA name _) = S.member name dvs
