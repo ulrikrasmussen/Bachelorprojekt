d3d86242be1d707bf3c7c2c5dd010ea2e54e6da9
Ulrik Rasmussen <ulrik@squish.(none)>
2010-05-11 13:58:52 +0200

    Optimiation: Synchronous API calls no longer leak continuations.

--- src/interp/Desugar.hs
+++ src/interp/Desugar.hs
@@ -39,11 +39,12 @@ desExp k (ConS n []) = return $ MsgA k [ConE n []]
 desExp k (ConS n es) =
   do chans <- getFresh $ length es
      vars <- getFresh $ length es
+     [once] <- getFresh 1
      es' <- zipWithM desExp chans es
      let joins = zipWith (\chan var -> VarJ chan [VarP var]) chans vars
-     return $ DefA [ReactionD joins
+     return $ DefA [ReactionD ((VarJ once []):joins)
                               (Proc [MsgA k [ConE n $ map VarE vars]])]
-                   (Proc es')
+                   (Proc $ (MsgA once []):es')
 
 -- | A call without any arguments is simply a message with the current
 -- continuation.
@@ -55,11 +56,12 @@ desExp k (CallS v []) = return $ MsgA v [VarE k]
 desExp k (CallS v es) =
   do chans <- getFresh $ length es
      vars <- getFresh $ length es
+     [once] <- getFresh 1
      let joins = zipWith (\chan var -> VarJ chan [VarP var]) chans vars
      es' <- zipWithM desExp chans es
-     return $ DefA [ReactionD joins
+     return $ DefA [ReactionD ((VarJ once []):joins)
                               (Proc [MsgA v (map VarE vars ++ [VarE k])])]
-                   (Proc $ es')
+                   (Proc $ (MsgA once []):es')
 
 -- |Desugaring of instruction lists. The continuation is used when desugaring
 --  match expressions: When the nested instruction list in a branch is finished,
@@ -75,28 +77,28 @@ desInstr mk [] = maybe (return [InertA])
 -- execution when they are all done.
 desInstr mk ((LetI pats e):is) =
   do is' <- desInstr mk is
-     [k] <- getFresh 1
+     [k, once] <- getFresh 2
      e' <- desExp k e
-     return $ [DefA [ReactionD [VarJ k pats] (Proc is')]
-                    (Proc [e'])]
+     return $ [DefA [ReactionD [VarJ once [], VarJ k pats] (Proc is')]
+                    (Proc [MsgA once [], e'])]
 
 desInstr mk ((RunI (Proc as)):is) = (as ++) <$> desInstr mk is
 
 -- |A do statement is executed by evaluating the given call expression, and
--- throwing away the result (hence the 'dummy' name).
+-- throwing away the result.
 desInstr mk ((DoI f es):is) =
   do is' <- desInstr mk is
-     [k, dummy] <- getFresh 2
+     [k, once] <- getFresh 2
      f' <- desExp k (CallS f es)
-     return $ [DefA [ReactionD [VarJ k [VarP dummy]] (Proc is')]
-                    (Proc [f'])]
+     return $ [DefA [ReactionD [VarJ once [], VarJ k []] (Proc is')]
+                    (Proc [MsgA once [], f'])]
 
 -- |When executing a match, we first evaluate the expression to be matched.
--- Each branch is given a continuation so it can signal when it is done
--- executing.
+-- Each branch is given the same continuation so they can signal when they are
+-- done executing.
 desInstr mk ((MatchI e mps):is) =
   do is' <- desInstr mk is
-     [l, m, var_e] <- getFresh 3
+     [l, m, var_e, once] <- getFresh 4
      e' <- desExp l e
      -- mk' is the continuation we give to each branch. If there are no
      -- instructions after this match instruction, we pass the current
@@ -105,11 +107,11 @@ desInstr mk ((MatchI e mps):is) =
      -- execution.
      let mk' = if (is == []) then mk else Just m
      mps' <- mapM (\(pat, is) -> (,) pat <$> (Proc <$> desInstr mk' is)) mps
-     return [DefA [ReactionD [VarJ l [VarP var_e]]
+     return [DefA [ReactionD [VarJ once [], VarJ l [VarP var_e]]
                              (Proc $ [MatchA (VarE var_e) mps'])
                   ,ReactionD [VarJ m []]
                              (Proc $ is')]
-                  (Proc [e'])]
+                  (Proc [MsgA once [], e'])]
 
 -- |When we return unit, simply send an empty message on the return-to
 -- continuation in parallel with executing the rest of the instructions.
@@ -131,11 +133,12 @@ desInstr mk ((ReturnI es f):is) =
   do is' <- desInstr mk is
      chans <- getFresh $ length es
      vars <- getFresh $ length es
+     [once] <- getFresh 1
      es' <- zipWithM desExp chans es
      let joins = zipWith (\chan var -> VarJ chan [VarP var]) chans vars
-     return [DefA [ReactionD joins
+     return [DefA [ReactionD ((VarJ once []):joins)
                              (Proc $ (MsgA (contName f) $ map VarE vars):is')]
-                  (Proc es')]
+                  (Proc $ (MsgA once []):es')]
 
 desAtom :: Atom -> DesugarM [Atom]
 desAtom (DefA defs (Proc as)) =
